47bf7252867990939f8c69ba37de7a902facc4a2
gabao55 <gabriel.s.rosin@gmail.com>
2022-09-13 17:00:55 -0300

    Setups project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => console.log("Listening to PORT 5000"));
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => console.log(`Listening to PORT ${process.env.PORT}`));
