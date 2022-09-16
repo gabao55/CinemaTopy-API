a399bc7da87b4a83ad61c3e98e073cb9dd3c4b58
gabao55 <gabriel.s.rosin@gmail.com>
2022-09-15 21:09:34 -0300

    Implements checkout for the purchase, to add address and to finish the purchase

---
+++ src/middlewares/address.middleware.js
@@ -0,0 +1,39 @@
+import joi from 'joi';
+
+const purchaseDetailsSchema = joi.object({
+    state: joi.string().required(),
+    street: joi.string().required(),
+    number: joi.number().integer().required(),
+    paymentMethod: joi.string().required(),
+    complement: joi.string().allow(null, ''),
+});
+
+const checkoutSchema = joi.object({
+    userId: joi.string().hex().length(24).required(),
+    products: joi.array().items(joi.object().keys({
+        productId: joi.string().hex().length(24).required(),
+        amount: joi.number().integer().min(1),
+    }).min(1).required()),
+});
+
+function validatePurchaseDetails(req, res, next) {
+    const validation = purchaseDetailsSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    next();
+}
+
+function validateCheckout(req, res, next) {
+    const validation = checkoutSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    next();
+}
+
+export { validatePurchaseDetails, validateCheckout };
---
+++ src/routes/checkout.router.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import authorizationMiddleware from '../middlewares/authorization.middleware.js'
+import { addUserPurchaseDetails, checkoutPurchase } from '../controllers/checkout.controller.js';
+import { validateCheckout, validatePurchaseDetails } from '../middlewares/address.middleware.js';
+
+const router = express.Router();
+
+router.use(authorizationMiddleware);
+
+router.put('/purchase-details', validatePurchaseDetails, addUserPurchaseDetails);
+router.post('/checkout', validateCheckout, checkoutPurchase);
+
+export default router;
