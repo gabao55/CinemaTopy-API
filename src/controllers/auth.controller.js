40e57e7e53105c564e71f27912e279b0902c8a59
gabao55 <gabriel.s.rosin@gmail.com>
2022-09-18 20:58:15 -0300

    Implements sending email after finishing the purchase order

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -18,7 +18,6 @@ const signInSchema = joi.object({
 
 const signUp = async (req, res) => {
   const { email, password } = req.body;
-  console.log('a')
 
   const validation = signUpSchema.validate(req.body, { abortEarly: false });
 
